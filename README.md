f8f297f7d15b1eb39950188118c06aee59a85873
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-03-16 23:30:04 +0300

    added gulp-gh-pages in dev dependencies

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Irvis Window (HTML/CSS/JQuery)
+# Irvis Window (HTML/CSS/JS)
 Website for ordering glazing of balconies and loggias
 
 ![run-smart](https://user-images.githubusercontent.com/62256669/111336951-b8da8380-8686-11eb-829c-143c1f7cb7b2.gif)
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
+const ghPages = require('gulp-gh-pages');
 
 const dist = "./dist/";
 
@@ -90,6 +91,10 @@ gulp.task("build-prod-js", () => {
                       }
                 }))
                 .pipe(gulp.dest(dist));
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*').pipe(ghPages());
 });
 
 gulp.task("default", gulp.parallel("watch", "build"));
